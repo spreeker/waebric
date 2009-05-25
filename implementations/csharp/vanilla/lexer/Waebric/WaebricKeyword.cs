fff855654ff945a6bd40d51d571c5fc1592e4d7c
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-05-25 14:20:50 +0000

    Added folder with waebric specific enumerations

---
+++ implementations/csharp/vanilla/lexer/Waebric/WaebricKeyword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Waebric
+{
+    public enum WaebricKeyword
+    {
+        IF, COMMENT, ECHO, CDATA, EACH, LET, YIELD, MODULE, IMPORT, DEF, END, SITE,
+    };
+}
