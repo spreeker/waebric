384885bb1598cb0b2135bce0c7d33700e41381f6
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-06-02 14:50:18 +0000

    Added methods to use NodeList in parsers

--- implementations/csharp/vanilla/parser/ast/NodeList.cs
+++ implementations/csharp/vanilla/parser/ast/NodeList.cs
@@ -21,5 +21,40 @@ namespace Parser.Ast
         {
             return list.Count;
         }
+
+        /// <summary>
+        /// Add element to NodeList
+        /// </summary>
+        /// <param name="element">Element to add</param>
+        public void Add(ISyntaxNode element)
+        {
+            list.Add(element);
+        }
+
+
+        /// <summary>
+        /// Get element from specified index
+        /// </summary>
+        /// <param name="index">Index of element to retrieve</param>
+        /// <returns>Element</returns>
+        public ISyntaxNode Get(int index)
+        {
+            return list.ElementAt(index);
+        }
+
+        public ISyntaxNode[] GetElements()
+        {
+            return list.ToArray();
+        }
+
+        /// <summary>
+        /// Clear the NodeList
+        /// </summary>
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+
     }
 }
