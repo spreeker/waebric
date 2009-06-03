56f093f4b46e42f29f19e72635ecee823932023e
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-06-03 09:04:52 +0000

    Additional public methods added to TokenIterator to make it useful for matching and peeking forward

--- implementations/csharp/vanilla/lexer/StreamTokenizer/TokenIterator.cs
+++ implementations/csharp/vanilla/lexer/StreamTokenizer/TokenIterator.cs
@@ -12,7 +12,7 @@ namespace Lexer.Tokenizer
     {
         #region Private Members
         private Token[] TokenArray; //Holds TokenStream as an array
-        private int Index = 0; //Index of iterator
+        private int Index = -1; //Index of iterator
         #endregion
 
         #region Public Methods
@@ -32,7 +32,7 @@ namespace Lexer.Tokenizer
         /// <returns>Token if available, otherwise null value</returns>
         public Token NextToken()
         {
-            if (Index > (TokenArray.Length - 1))
+            if (Index > TokenArray.Length)
             {
                 return null; //Index out of bound, end of array reached
             }
@@ -50,7 +50,36 @@ namespace Lexer.Tokenizer
         /// <returns>HasNext Token in Stream</returns>
         public bool HasNext()
         {
-            return (Index > (TokenArray.Length - 1));
+            return HasNext(1);
+        }
+
+        /// <summary>
+        /// Poll if there is a token on specified offset of current index
+        /// </summary>
+        /// <param name="index">Offset of current index</param>
+        /// <returns>True if token on index+offset, false if not</returns>
+        public bool HasNext(int offset)
+        {
+            return ((this.Index + offset) > TokenArray.Length);
+        }
+
+        /// <summary>
+        /// Peek a token on specified offset of current index
+        /// </summary>
+        /// <param name="index">Offset of current index</param>
+        /// <returns>Token if token on index.offset, null if no token exists</returns>
+        /// <remarks>Check with HasNext(offset) before try'ing to retrieve token</remarks>
+        public Token Peek(int offset)
+        {
+            //Check if there's something on the specified index
+            if (HasNext(offset))
+            {
+                return TokenArray[(this.Index + offset)];
+            }
+            else
+            {
+                return null; //Index out of bound
+            }
         }
 
         #endregion
