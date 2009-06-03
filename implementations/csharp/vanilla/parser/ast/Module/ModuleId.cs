3f8ed29f65a8a0ae202b6d67deee41c628ca753d
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-06-03 11:31:52 +0000

    Implemented ModuleId

--- implementations/csharp/vanilla/parser/ast/Module/ModuleId.cs
+++ implementations/csharp/vanilla/parser/ast/Module/ModuleId.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Parser.Ast.Basic;
 
 namespace Parser.Ast.Module
 {
@@ -10,5 +11,32 @@ namespace Parser.Ast.Module
     /// </summary>
     public class ModuleId : ISyntaxNode
     {
+        #region Private Members
+
+        private IdentifierCon Identifier;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Get identifier
+        /// </summary>
+        /// <returns>Identifier</returns>
+        public IdentifierCon GetIdentifier()
+        {
+            return Identifier;
+        }
+
+        /// <summary>
+        /// Set identifier
+        /// </summary>
+        /// <param name="identifier">Identifier to set</param>
+        public void SetIdentifier(IdentifierCon identifier)
+        {
+            this.Identifier = identifier;   
+        }
+
+        #endregion
     }
 }
