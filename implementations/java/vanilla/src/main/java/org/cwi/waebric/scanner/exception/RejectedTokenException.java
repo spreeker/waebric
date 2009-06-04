910b237f695dfb8f0b8f746f3a9b57702a34e52c
jeroen.v.schagen <jeroen.v.schagen@gmail.com>
2009-06-04 22:26:40 +0000

    rejection exception added to provide lexical validation on tokens that should not be accepted

---
+++ implementations/java/vanilla/src/main/java/org/cwi/waebric/scanner/exception/RejectedTokenException.java
@@ -0,0 +1,20 @@
+package org.cwi.waebric.scanner.exception;
+
+/**
+ * {reject}
+ * 
+ * @author Jeroen van Schagen
+ * @date 05-06-2009
+ */
+public class RejectedTokenException extends ScannerException {
+	
+	/**
+	 * Serial ID
+	 */
+	private static final long serialVersionUID = 5564792979377038948L;
+
+	public RejectedTokenException(String message) {
+		super("");
+	}
+
+}
