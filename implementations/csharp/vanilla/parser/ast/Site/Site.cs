416ee09bf3ef1a65bf673635ab7f520144a92617
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-06-08 13:12:47 +0000

    Implemented site class

--- implementations/csharp/vanilla/parser/ast/Site/Site.cs
+++ implementations/csharp/vanilla/parser/ast/Site/Site.cs
@@ -2,13 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Parser.Ast.Module;
 
 namespace Parser.Ast.Site
 {
     /// <summary>
-    /// Site Node which contains a site
+    /// Site Node
     /// </summary>
-    public class Site : ISyntaxNode
+    public class Site : ModuleElement
     {
+        #region Private Members
+
+        private MappingList Mappings;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Get mappings of site
+        /// </summary>
+        /// <returns>MappingList</returns>
+        public MappingList GetMappings()
+        {
+            return Mappings;
+        }
+
+        /// <summary>
+        /// Set mappings of site
+        /// </summary>
+        /// <param name="mappings">MappingList</param>
+        public void SetMappings(MappingList mappings)
+        {
+            this.Mappings = mappings;
+        }
+
+        #endregion
+
     }
 }
