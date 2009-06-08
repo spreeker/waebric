2afd9eef8b97cb64d703e0d7f680339947de7d4b
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-06-08 13:15:23 +0000

    Added extra tests to moduleparser but detected bug in Tokenizer. Still needs to being fixed before paths can parsed correctly.

--- implementations/csharp/vanilla/TestParser/ModuleParserTest.cs
+++ implementations/csharp/vanilla/TestParser/ModuleParserTest.cs
@@ -71,7 +71,7 @@ namespace TestParser
 
 
         /// <summary>
-        ///A test for ModuleParser Constructor
+        ///Test Module Parser with single module definition
         ///</summary>
         [TestMethod()]
         public void ModuleParserSingleModuleTest()
@@ -99,7 +99,69 @@ namespace TestParser
             Assert.IsTrue(modules.GetSize() == 1); //Contains only 1 module
 
             Module module = (Module) modules.Get(0);
-            Assert.IsTrue(module.GetIdentifier().ToString() == "test");
+            Assert.IsTrue(module.GetIdentifier().GetIdentifier().GetIdCon().ToString() == "test");
+        }
+
+        /// <summary>
+        /// Test Module with import
+        /// </summary>
+        [TestMethod]
+        public void ModuleParserImportTest()
+        {
+            List<Exception> exceptions;
+            SyntaxTree tree;
+
+            //Create lexer to tokenize stream
+            WaebricLexer lexer = new WaebricLexer(new StringReader("module test\n\nimport importtest"));
+            lexer.LexicalizeStream();
+
+            //Test if stream is lexicalized into 4 tokens
+            Assert.IsTrue(lexer.GetTokenIterator().GetSize() == 4);
+
+            //Retrieve tokenIterator from lexer and lets parse it
+            WaebricParser parser = new WaebricParser(lexer.GetTokenIterator());
+            exceptions = parser.Parse();
+
+            //No exceptions should occur
+            Assert.IsTrue(exceptions.Count == 0);
+
+            //Test tree structure
+            tree = parser.GetTree();
+
+            ModuleList modules = (ModuleList)tree.GetRoot();
+            Assert.IsTrue(modules.GetSize() == 1); //Just one module in list
+            Assert.IsTrue(modules.GetElements().Length == 1); //One element
+            Module module = (Module) modules.Get(0);
+
+            ISyntaxNode[] moduleElements = module.GetElements();
+
+            Assert.IsTrue(moduleElements[0].GetType() == typeof(Import));
+
+            Import import = (Import) moduleElements[0];
+            //TODO: As shown here it takes to much methods to get real identifier
+            Assert.IsTrue(import.GetIdentifier().GetIdentifier().GetIdCon().ToString() == "importtest");
+        }
+
+        /// <summary>
+        /// Test module with site 
+        /// </summary>
+        [TestMethod]
+        public void ModuleParserSiteTest()
+        {
+            List<Exception> exceptions;
+            SyntaxTree tree;
+
+            //Create lexer to tokenize stream
+            WaebricLexer lexer = new WaebricLexer(new StringReader("module test\n\nsite\n  site/index.html : home()\nend"));
+            lexer.LexicalizeStream();
+
+            //Test if stream is lexicalized into 4 tokens
+            //Assert.IsTrue(lexer.GetTokenIterator().GetSize() == 4);
+
+            //Retrieve tokenIterator from lexer and lets parse it
+            //WaebricParser parser = new WaebricParser(lexer.GetTokenIterator());
+            //exceptions = parser.Parse();
+
         }
     }
 }
