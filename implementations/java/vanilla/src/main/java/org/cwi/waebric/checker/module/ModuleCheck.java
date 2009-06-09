84f89b2d8238bfeff32527cbab52fac1a91646bf
jeroen.v.schagen <jeroen.v.schagen@gmail.com>
2009-06-09 12:17:52 +0000

    module exception embedded in check!

--- implementations/java/vanilla/src/main/java/org/cwi/waebric/checker/module/ModuleCheck.java
+++ implementations/java/vanilla/src/main/java/org/cwi/waebric/checker/module/ModuleCheck.java
@@ -90,4 +90,24 @@ public class ModuleCheck implements IWaebricCheck {
 		return path;
 	}
 	
+	/**
+	 * If for an import directive import m no corresponding file m.wae 
+	 * can be found, this a an error. [The import directive is skipped]
+	 * 
+	 * @author Jeroen van Schagen
+	 * @date 09-06-2009
+	 */
+	public class NonExistingModuleException extends SemanticException {
+
+		/**
+		 * Generated serial ID
+		 */
+		private static final long serialVersionUID = -4503945323554024642L;
+
+		public NonExistingModuleException(ModuleId id) {
+			super(id.toString() + " is a non-existing module.");
+		}
+		
+	}
+	
 }
