1b7c322014be3b6ffad7fa67dad139d992d02065
jeroen.v.schagen <jeroen.v.schagen@gmail.com>
2009-06-09 12:24:02 +0000

    all exceptionsa re now embedded in their related checks

---
+++ implementations/java/vanilla/src/main/java/org/cwi/waebric/checker/FunctionCheck.java
@@ -0,0 +1,70 @@
+package org.cwi.waebric.checker;
+
+import org.cwi.waebric.parser.ast.markup.Markup;
+import org.cwi.waebric.parser.ast.module.FunctionDef;
+
+public class FunctionCheck {
+
+	/**
+	 * If a function is called with an incorrect number of arguments 
+	 * (as follows from its definition), this is an error.
+	 * 
+	 * @author Jeroen van Schagen
+	 * @date 09-06-2009
+	 */
+	public class ArityMismatchException extends SemanticException {
+
+		/**
+		 * Generated serial ID
+		 */
+		private static final long serialVersionUID = -954167103131401047L;
+
+		public ArityMismatchException(Markup.Call call) {
+			super(call.toString() + " is an arity mismatch");
+		}
+		
+	}
+	
+	/**
+	 * Multiple function definitions with the same name are disallowed.
+	 * 
+	 * @author Jeroen van Schagen
+	 * @date 09-06-2009
+	 */
+	public class DuplicateFunctionDefinition extends SemanticException {
+
+		/**
+		 * Generated serial ID
+		 */
+		private static final long serialVersionUID = -8833578229100261366L;
+
+		public DuplicateFunctionDefinition(FunctionDef def) {
+			super(def.toString() + " is a duplicate function definition");
+		}
+		
+	}
+	
+	/**
+	 * If for a markup-call (f) no function definition can be found in 
+	 * the current module or one of its (transitive) imports, and, if 
+	 * f is not a tag defined in the XHTML 1.0 Transitional standard, 
+	 * then this is an error. [f will be interpreted as if it was part 
+	 * of XHTML 1.0 transitional.]
+	 * 
+	 * @author Jeroen van Schagen
+	 * @date 09-06-2009
+	 */
+	public class UndefinedFunctionException extends SemanticException {
+
+		/**
+		 * Generated serial ID
+		 */
+		private static final long serialVersionUID = -4467095005921534334L;
+
+		public UndefinedFunctionException(Markup.Call call) {
+			super(call.toString() + " is an undefined function.");
+		}
+		
+	}
+	
+}
