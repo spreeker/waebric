e0507ef621e56e77e8c9597f4fd538e3db2c4e46
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-06-22 08:00:37 +0000

    Simplified formals classes

--- implementations/csharp/vanilla/parser/ast/Functions/Formal.cs
+++ implementations/csharp/vanilla/parser/ast/Functions/Formal.cs
@@ -10,5 +10,42 @@ namespace Parser.Ast.Functions
     /// </summary>
     public class Formal : ISyntaxNode
     {
+        #region Private Members
+
+        private String Identifier;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Set identifier of Formal
+        /// </summary>
+        /// <param name="identifier">Identifier to set</param>
+        public void SetIdentifier(String identifier)
+        {
+            Identifier = identifier;
+        }
+
+        /// <summary>
+        /// Get identifier of Formal
+        /// </summary>
+        /// <returns>Identifier</returns>
+        public String GetIdentifier()
+        {
+            return Identifier;
+        }
+
+        /// <summary>
+        /// Get string representation of Formal
+        /// </summary>
+        /// <returns>String</returns>
+        public override String ToString()
+        {
+            return Identifier;
+        }
+
+        #endregion
+
     }
 }
