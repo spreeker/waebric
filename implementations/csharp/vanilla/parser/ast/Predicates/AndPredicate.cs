8b08ddcbd2d5e5409a6a6badffc6f6634aa63ea2
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-06-22 10:10:20 +0000

    Added empty predicate classes

---
+++ implementations/csharp/vanilla/parser/ast/Predicates/AndPredicate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parser.Ast.Predicates
+{
+    /// <summary>
+    /// Node which contains an AndPredicate
+    /// </summary>
+    public class AndPredicate : Predicate
+    {
+    }
+}
---
+++ implementations/csharp/vanilla/parser/ast/Predicates/NotPredicate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parser.Ast.Predicates
+{
+    /// <summary>
+    /// Node which contains an NotPredicate
+    /// </summary>
+    public class NotPredicate : Predicate
+    {
+    }
+}
