ab031cddade850a80523f48feb9c68ef21ce70e1
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-06-23 15:08:58 +0000

    Added empty embedding parser

---
+++ implementations/csharp/vanilla/parser/EmbeddingParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lexer.Tokenizer;
+
+namespace Parser
+{
+    public class EmbeddingParser : AbstractParser
+    {
+        #region Private Members
+
+        MarkupParser markupParser;
+
+        #endregion
+
+        #region Public Methods
+
+        public EmbeddingParser(TokenIterator tokenStream, List<Exception> exceptionList)
+            : base(tokenStream, exceptionList)
+        {
+            //Create subparser
+            markupParser = new MarkupParser(tokenStream, exceptionList);
+        }
+
+        #endregion
+    }
+}
