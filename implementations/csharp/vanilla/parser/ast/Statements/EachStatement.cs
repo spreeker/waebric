21b90cb03ce10a61eadd1071af8e1a5345c7ddf7
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-06-23 12:52:57 +0000

    Fixed build problems with some statement classes

--- implementations/csharp/vanilla/parser/ast/Statements/EachStatement.cs
+++ implementations/csharp/vanilla/parser/ast/Statements/EachStatement.cs
@@ -15,35 +15,73 @@ namespace Parser.Ast.Statements
 
         private String Identifier;
         private Expression Expression;
-        private Statement EachStatement;
+        private Statement Statement;
 
         #endregion
 
         #region Public Methods
 
+        /// <summary>
+        /// Set identifier of EachStatement
+        /// </summary>
+        /// <param name="identifier">Identifier to set</param>
         public void SetIdentifier(String identifier)
         {
             Identifier = identifier;
         }
 
+        /// <summary>
+        /// Get identifier of EachStatement
+        /// </summary>
+        /// <returns>Identifier</returns>
         public String GetIdentifier()
         {
             return Identifier;
         }
 
+        /// <summary>
+        /// Set expression of EachStatement
+        /// </summary>
+        /// <param name="expression">Expression to set</param>
         public void SetExpression(Expression expression)
         {
             Expression = expression;
         }
 
+        /// <summary>
+        /// Get expression of EachStatement
+        /// </summary>
+        /// <returns>Expression</returns>
         public Expression GetExpression()
         {
             return Expression;
         }
 
+        /// <summary>
+        /// Set statement of EachStatement
+        /// </summary>
+        /// <param name="statement">Statement to set</param>
+        public void SetStatement(Statement statement)
+        {
+            Statement = statement;
+        }
+
+        /// <summary>
+        /// Get statement of EachStatement
+        /// </summary>
+        /// <returns>Statement</returns>
+        public Statement GetStatement()
+        {
+            return Statement;
+        }
+
+        /// <summary>
+        /// Get string representation of EachStatement
+        /// </summary>
+        /// <returns>String</returns>
         public override String ToString()
         {
-            return "each (" + Identifier + ":" + Expression.ToString() + ")" + EachStatement.ToString();
+            return "each (" + Identifier + ":" + Expression.ToString() + ")" + Statement.ToString();
         }
 
         #endregion
