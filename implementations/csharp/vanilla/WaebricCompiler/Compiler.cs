e54b82dedb8553c3bc9df25ba5c4cd4a2735b68d
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-06-30 09:12:39 +0000

    Added interpreter project, changed all projects to class library's and created start point for compiler

--- implementations/csharp/vanilla/WaebricCompiler/Compiler.cs
+++ implementations/csharp/vanilla/WaebricCompiler/Compiler.cs
@@ -5,10 +5,17 @@ using System.Text;
 
 namespace WaebricCompiler
 {
+    /// <summary>
+    /// Class which contains the main program
+    /// </summary>
     class Compiler
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Waebric Compiler/Interpreter v1.0");
+
+
+            
         }
     }
 }
