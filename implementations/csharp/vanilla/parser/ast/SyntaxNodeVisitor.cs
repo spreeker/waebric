7bc823cb9aa88ea545d8d886992aa21798e9721d
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-07-09 10:27:45 +0000

    Made Visit methods virtual to make Vistitors work correctly

--- implementations/csharp/vanilla/parser/ast/SyntaxNodeVisitor.cs
+++ implementations/csharp/vanilla/parser/ast/SyntaxNodeVisitor.cs
@@ -21,332 +21,332 @@ namespace Parser.Ast
         #region Public Methods
 
         //All visit methods for all kind of NodeTypes
-        public void Visit(Module.Module module)
+        public virtual void Visit(Module.Module module)
         {
             VisitSubNodes(module);
         }
 
-        public void Visit(ModuleId module)
+        public virtual void Visit(ModuleId module)
         {
             VisitSubNodes(module);
         }
 
-        public void Visit(Import import)
+        public virtual void Visit(Import import)
         {
             VisitSubNodes(import);
         }
 
-        public void Visit(Site.Site site)
+        public virtual void Visit(Site.Site site)
         {
             VisitSubNodes(site);
         }
 
-        public void Visit(Mapping mapping)
+        public virtual void Visit(Mapping mapping)
         {
             VisitSubNodes(mapping);
         }
 
-        public void Visit(Directory directory)
+        public virtual void Visit(Directory directory)
         {
             VisitSubNodes(directory);   
         }
 
-        public void Visit(DirName dirName)
+        public virtual void Visit(DirName dirName)
         {
             VisitSubNodes(dirName);
         }
 
-        public void Visit(FileExt fileExt)
+        public virtual void Visit(FileExt fileExt)
         {
             VisitSubNodes(fileExt);
         }
 
-        public void Visit(FileName fileName)
+        public virtual void Visit(FileName fileName)
         {
             VisitSubNodes(fileName);
         }
 
-        public void Visit(Path path)
+        public virtual void Visit(Path path)
         {
             VisitSubNodes(path);
         }
 
-        public void Visit(PathElement pathElement)
+        public virtual void Visit(PathElement pathElement)
         {
             VisitSubNodes(pathElement);
         }
 
-        public void Visit(FunctionDefinition functionDefinition)
+        public virtual void Visit(FunctionDefinition functionDefinition)
         {
             VisitSubNodes(functionDefinition);
         }
 
-        public void Visit(Formal formal)
+        public virtual void Visit(Formal formal)
         {
             VisitSubNodes(formal);
         }
 
-        public void Visit(BlockStatement statement)
+        public virtual void Visit(BlockStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(CdataStatement statement)
+        public virtual void Visit(CdataStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(CommentStatement statement)
+        public virtual void Visit(CommentStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(EachStatement statement)
+        public virtual void Visit(EachStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(EchoEmbeddingStatement statement)
+        public virtual void Visit(EchoEmbeddingStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(EchoExpressionStatement statement)
+        public virtual void Visit(EchoExpressionStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(IfElseStatement statement)
+        public virtual void Visit(IfElseStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(IfStatement statement)
+        public virtual void Visit(IfStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(LetStatement statement)
+        public virtual void Visit(LetStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(YieldStatement statement)
+        public virtual void Visit(YieldStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(FuncBindAssignment assignment)
+        public virtual void Visit(FuncBindAssignment assignment)
         {
             VisitSubNodes(assignment);
         }
 
-        public void Visit(VarBindAssignment assignment)
+        public virtual void Visit(VarBindAssignment assignment)
         {
             VisitSubNodes(assignment);
         }
 
-        public void Visit(MarkupStatStatement statement)
+        public virtual void Visit(MarkupStatStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(MarkupStatement statement)
+        public virtual void Visit(MarkupStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(MarkupMarkupStatement statement)
+        public virtual void Visit(MarkupMarkupStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(MarkupExpressionStatement statement)
+        public virtual void Visit(MarkupExpressionStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(MarkupEmbeddingStatement statement)
+        public virtual void Visit(MarkupEmbeddingStatement statement)
         {
             VisitSubNodes(statement);
         }
 
-        public void Visit(AndPredicate predicate)
+        public virtual void Visit(AndPredicate predicate)
         {
             VisitSubNodes(predicate);
         }
 
-        public void Visit(ExpressionPredicate predicate)
+        public virtual void Visit(ExpressionPredicate predicate)
         {
             VisitSubNodes(predicate);
         }
 
-        public void Visit(IsPredicate predicate)
+        public virtual void Visit(IsPredicate predicate)
         {
             VisitSubNodes(predicate);
         }
 
-        public void Visit(ListType type)
+        public virtual void Visit(ListType type)
         {
             VisitSubNodes(type);
         }
 
-        public void Visit(NotPredicate predicate)
+        public virtual void Visit(NotPredicate predicate)
         {
             VisitSubNodes(predicate);
         }
 
-        public void Visit(OrPredicate predicate)
+        public virtual void Visit(OrPredicate predicate)
         {
             VisitSubNodes(predicate);
         }
 
-        public void Visit(RecordType type)
+        public virtual void Visit(RecordType type)
         {
             VisitSubNodes(type);
         }
 
-        public void Visit(StringType type)
+        public virtual void Visit(StringType type)
         {
             VisitSubNodes(type);
         }
 
-        public void Visit(AttrArgument argument)
+        public virtual void Visit(AttrArgument argument)
         {
             VisitSubNodes(argument);
         }
 
-        public void Visit(ClassAttribute attribute)
+        public virtual void Visit(ClassAttribute attribute)
         {
             VisitSubNodes(attribute);
         }
 
-        public void Visit(Designator designator)
+        public virtual void Visit(Designator designator)
         {
             VisitSubNodes(designator);
         }
 
-        public void Visit(ExpressionArgument argument)
+        public virtual void Visit(ExpressionArgument argument)
         {
             VisitSubNodes(argument);
         }
 
-        public void Visit(HeightAttribute attribute)
+        public virtual void Visit(HeightAttribute attribute)
         {
             VisitSubNodes(attribute);
         }
 
-        public void Visit(IdAttribute attribute)
+        public virtual void Visit(IdAttribute attribute)
         {
             VisitSubNodes(attribute);
         }
 
-        public void Visit(Markup.Markup markup)
+        public virtual void Visit(Markup.Markup markup)
         {
             VisitSubNodes(markup);
         }
 
-        public void Visit(NameAttribute attribute)
+        public virtual void Visit(NameAttribute attribute)
         {
             VisitSubNodes(attribute);
         }
 
-        public void Visit(TypeAttribute attribute)
+        public virtual void Visit(TypeAttribute attribute)
         {
             VisitSubNodes(attribute);
         }
 
-        public void Visit(Width_HeightAttribute attribute)
+        public virtual void Visit(Width_HeightAttribute attribute)
         {
             VisitSubNodes(attribute);
         }
 
-        public void Visit(CatExpression expression)
+        public virtual void Visit(CatExpression expression)
         {
             VisitSubNodes(expression);
         }
 
-        public void Visit(FieldExpression expression)
+        public virtual void Visit(FieldExpression expression)
         {
             VisitSubNodes(expression);
         }
 
-        public void Visit(KeyValuePair keyValuePair)
+        public virtual void Visit(KeyValuePair keyValuePair)
         {
             VisitSubNodes(keyValuePair);
         }
 
-        public void Visit(ListExpression expression)
+        public virtual void Visit(ListExpression expression)
         {
             VisitSubNodes(expression);
         }
 
-        public void Visit(NumExpression expression)
+        public virtual void Visit(NumExpression expression)
         {
             VisitSubNodes(expression);
         }
 
-        public void Visit(RecordExpression expression)
+        public virtual void Visit(RecordExpression expression)
         {
             VisitSubNodes(expression);
         }
 
-        public void Visit(SymExpression expression)
+        public virtual void Visit(SymExpression expression)
         {
             VisitSubNodes(expression);
         }
 
-        public void Visit(TextExpression expression)
+        public virtual void Visit(TextExpression expression)
         {
             VisitSubNodes(expression);
         }
 
-        public void Visit(VarExpression expression)
+        public virtual void Visit(VarExpression expression)
         {
             VisitSubNodes(expression);
         }
 
-        public void Visit(Embedding.Embedding embedding)
+        public virtual void Visit(Embedding.Embedding embedding)
         {
             VisitSubNodes(embedding);
         }
 
-        public void Visit(ExpressionEmbed embed)
+        public virtual void Visit(ExpressionEmbed embed)
         {
             VisitSubNodes(embed);
         }
 
-        public void Visit(MarkupEmbed embed)
+        public virtual void Visit(MarkupEmbed embed)
         {
             VisitSubNodes(embed);
         }
 
-        public void Visit(MidTextTail textTail)
+        public virtual void Visit(MidTextTail textTail)
         {
             VisitSubNodes(textTail);
         }
 
-        public void Visit(PostTextTail textTail)
+        public virtual void Visit(PostTextTail textTail)
         {
             VisitSubNodes(textTail);
         }
 
-        public void Visit(PreText preText)
+        public virtual void Visit(PreText preText)
         {
             VisitSubNodes(preText);
         }
 
-        public void Visit(MidText midText)
+        public virtual void Visit(MidText midText)
         {
             VisitSubNodes(midText);
         }
 
-        public void Visit(PostText postText)
+        public virtual void Visit(PostText postText)
         {
             VisitSubNodes(postText);
         }
 
-        public void Visit(NodeList list)
+        public virtual void Visit(NodeList list)
         {
             VisitSubNodes(list);
         }
