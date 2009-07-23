384ab74cced5706cc9e73893c765fd67af846dd9
jeroenvanlieshout <jeroenvanlieshout@gmail.com>
2009-07-23 05:51:20 +0000

    Added forgotten tokenexception file

---
+++ implementations/csharp/vanilla/parser/Exceptions/UnexpectedToken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lexer.Tokenizer;
+
+namespace Parser.Exceptions
+{
+    /// <summary>
+    /// Class which implements an UnexpectedToken exception
+    /// </summary>
+    public class UnexpectedToken : Exception
+    {
+
+        public UnexpectedToken(String message, String found, int location)
+        {
+            Console.WriteLine(message + " " + found + " at line " + location.ToString());
+        }
+    }
+}
