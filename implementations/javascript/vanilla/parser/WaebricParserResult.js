2daf51986f8fe3edaed8c46bad5bdf23233f667e
nickolas <djnickolas1@gmail.com>
2009-08-08 00:21:13 +0000

    Parser outputs WaebricParserResult

---
+++ implementations/javascript/vanilla/parser/WaebricParserResult.js
@@ -0,0 +1,10 @@
+/**
+ * Holds the result of the parser
+ * 
+ * @param {Object} module
+ * @param {Object} exceptions
+ */
+function WaebricParserResult(module, exceptions){
+	this.module = module;
+	this.exceptions = exceptions;
+}
