9e7baf9d5ca26a847b902b02522250aa1d7477c8
spreeker <spreeker@gmail.com>
2009-11-20 15:19:00 +0000

    added seperate keywords file

---
+++ implementations/python/vanilla/parser/keywords.py
@@ -0,0 +1,11 @@
+keywords = {'IF':'if', 'ELSE':'else',
+            'EACH':'each', 'LET':'let', 'IN':'in', 'COMMENT':'comment',
+            'ECHO':'echo', 'CDATA':'cdata', 'YIELD':'yield',
+
+            'MODULE':'module', 'IMPORT':'import', 'DEF':'def', 'END':'end',
+            'SITE':'site',
+
+            'LIST':'list', 'RECORD':'record', 'STRING':'string',
+            }
+
+
