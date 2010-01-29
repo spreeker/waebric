b5b63b0d67f88413010880b84aee34336ad6c2a2
spreeker <spreeker@gmail.com>
2010-01-29 09:10:36 +0000

    interpreter scaffolding

--- implementations/python/vanilla/interpreter/interpreter.py
+++ implementations/python/vanilla/interpreter/interpreter.py
@@ -8,16 +8,62 @@ Generates output of Astract Syntac Tree of waebrick parse Tree.
 """
 from parser import parser
 
-def interprete(module):
-    #create envitonment.
-    #parse source and get AST.
-    #interpretate AST
-    #create output.? 
+import elementtree.ElementTree as ET #change to cElementree
+#if possible.
+
+
+class Interpreter(object):
+    """interpret waebric ast """
+    output = ""
+    #keep track of dependencies/modules
+
+    def interprete(module):
+        #create envitonment.
+        #parse source and get AST.
+        #interpretate AST
+        #create output.?
+
+        #load modules.
+        #load sites
+        #execute site main functions.
+        #write output.
+        pass
+
+    def getDependencies(env,ast):
+        pass
+
+    def getSiteMappigns(env, ast):
+        pass
+
+    def getEnvMainFunction(env,ast):
+        pass
+
+
+class Environment(object):
+    """ keeps track of environment functions and variables """
+
+    def __init__(self):
+        self.variables = {}
+        self.functions = {}
+
+    def __repr__(self):
+        vars = "".join(["%s:%s" % (var, value) for var in self.variables.items()])
+        functions = "".join(self.functions.keys())
+
+        return "Variables %s, \n Functions %s" % (vars,functions)
+
+
+class UndefinedFunction(exception):
+    pass
+
+class UndefinedVariable(exception):
+    pass
+
+class UndefinedModule(exception):
+    pass
+
+class ArityMismatch(exception):
+    """ function is called with wrong arguments."""
     pass
 
 
-def getSiteMappigns(env, ast):
-    pass
-
-
-def getEnvMainFunction(env,ast):
